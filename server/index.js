1b30eda9499bf30a8f93d6f79b9e24b744a31794
Ishan <Braviaspan2002@gmail.com>
2023-12-20 01:11:40 +0530

    Fixed minor bugs

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use("/api/auth",require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.json('Hello World')
 })
 
 app.listen(port, async () => {
