1577c96c085356023d8a50b3dfbfec205597fc9f
Ishan <Braviaspan2002@gmail.com>
2023-12-30 22:25:50 +0530

    Disables dev tools in prod

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import UserState from './context/UserState.jsx'
 
-
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
