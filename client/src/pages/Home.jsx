40435e4a4d2d37e5493d2e1f81acf85fa128261d
Ishan <Braviaspan2002@gmail.com>
2023-12-31 14:27:22 +0530

    Added loading for getting all notes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,14 +3,22 @@ import Search from '../components/Search';
 import { Outlet, useOutletContext } from 'react-router-dom';
 import noteContext from '../context/noteContext';
 import Footer from '../components/Footer';
+import { Spinner } from "@material-tailwind/react";
 
 const Home = () => {
     const [searchText, setSearchText] = useState("")
     const expanded = useOutletContext()
     const { notes, getAllNotes } = useContext(noteContext)
+    const [loading, setLoading] = useState(false)
+
+    const getNotes = async () =>{
+        setLoading(true)
+        await getAllNotes()
+        setLoading(false)
+    }
 
     useEffect(() => {
-        getAllNotes()
+        getNotes()
     }, [])
 
     return (
@@ -18,9 +26,16 @@ const Home = () => {
                 <div className='flex my-4 justify-between '>
                     <Search searchText={searchText} setSearchText={setSearchText} />
                 </div>
-                <div className='flex-1'>
+                <div className=' flex-1'>
                 {
-                    notes.length === 0
+                    loading
+                    ?<div className='flex h-full justify-center items-center '>
+                        <div className='bg-white dark:bg-gray-900 flex flex-col h-32 w-32 z-50 justify-center items-center rounded-xl gap-3'>
+                        <Spinner className="h-12 w-12" />
+                        <span className='uppercase font-bold'>Loading..</span>
+                        </div>
+                    </div>
+                    :notes.length === 0
                         ? <div className='flex  justify-center pt-16'>
                             <span className='text-3xl sm:text-5xl font-black text-gray-600 text-center'>
                                 What's on your mind....
